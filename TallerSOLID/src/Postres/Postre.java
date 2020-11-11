ef6e2a5e08dbc47acd97fa27b7f0584866f8a256
User <hatuspellegrini1@gmail.com>
2020-11-11 16:21:39 -0500

    Eliminada la clase redundante OperacionesAderezo y remplazada por dos metodos en la clase padre postre.

--- TallerSOLID/src/Postres/Postre.java
+++ TallerSOLID/src/Postres/Postre.java
@@ -4,19 +4,20 @@ import java.util.ArrayList;
 import Adicionales.Aderezo;
 
 public class Postre {
-	private String sabor;
+
+    private String sabor;
     private double precioParcial;
     private ArrayList<Aderezo> aderezos;
-    
-    public Postre(String sabor){
-        aderezos= new ArrayList<>();
-        this.sabor=sabor;
-        
+
+    public Postre(String sabor) {
+        aderezos = new ArrayList<>();
+        this.sabor = sabor;
+
     }
-    
-    public double calcularPrecioFinal(){
+
+    public double calcularPrecioFinal() {
         double precioFinal;
-        precioFinal=(precioParcial+(precioParcial*0.12))+(aderezos.size()*0.50);
+        precioFinal = (precioParcial + (precioParcial * 0.12)) + (aderezos.size() * 0.50);
         return precioFinal;
     }
 
@@ -24,27 +25,32 @@ public class Postre {
         return aderezos;
     }
 
-	public String getSabor() {
-		return sabor;
-	}
+    public String getSabor() {
+        return sabor;
+    }
 
-	public void setSabor(String sabor) {
-		this.sabor = sabor;
-	}
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
 
-	public double getPrecioParcial() {
-		return precioParcial;
-	}
+    public double getPrecioParcial() {
+        return precioParcial;
+    }
 
-	public void setPrecioParcial(double precioParcial) {
-		this.precioParcial = precioParcial;
-	}
+    public void setPrecioParcial(double precioParcial) {
+        this.precioParcial = precioParcial;
+    }
 
-	public void setAderezos(ArrayList<Aderezo> aderezos) {
-		this.aderezos = aderezos;
-	}
-
+    public void setAderezos(ArrayList<Aderezo> aderezos) {
+        this.aderezos = aderezos;
+    }
     
     
+    public void anadirAderezo(Aderezo aderezo){
+        this.aderezos.add(aderezo);
+    }
     
+    public void removerAderezo(Aderezo aderezo){
+        this.aderezos.remove(aderezo);
+    }
 }
--- TallerSOLID/src/Principal/Sistema.java
+++ TallerSOLID/src/Principal/Sistema.java
@@ -4,39 +4,42 @@
  * and open the template in the editor.
  */
 package Principal;
+
 import Adicionales.Aderezo;
 import Postres.*;
 import Procesos.*;
 //import Leche.*;
+
 /**
  *
  * @author djurado
  */
 public class Sistema {
-    public static void main(String [ ] args){
+
+    public static void main(String[] args) {
         // Producir un helado de vainilla y una torta de chocolate, 
         // a ambos agregarles CREMA y FRUTILLAS
         // y cambiar el tipo de leche por Leche Descremada
         // Finalmente mostrar el precio final de cada uno
         //LecheEntera leche = new LecheDeslactosada();
         //ManejadorDeLeche mnj_leche = new ManejadorDeLeche();
-        
+
         // Producir Helado
         Helado helado_vainilla = new Helado("Vainilla");
-        OperacionesAderezo.anadirAderezoHelado(helado_vainilla, Aderezo.CREMA);
-        OperacionesAderezo.anadirAderezoHelado(helado_vainilla, Aderezo.FRUTILLA);
+        helado_vainilla.anadirAderezo(Aderezo.CREMA);
+        helado_vainilla.anadirAderezo(Aderezo.FRUTILLA);
         System.out.println(helado_vainilla);
 //        mnj_leche.cambiarTipoLeche(leche, helado_vainilla);
         System.out.println(helado_vainilla.showPrecioFinal());
-        
+
         // Producir Pastel
         Pastel pastel_chocolate = new Pastel("Chocolate");
-        OperacionesAderezo.quitarAderezoPastel(pastel_chocolate, Aderezo.CREMA);
-        OperacionesAderezo.anadirAderezoPastel(pastel_chocolate, Aderezo.FRUTILLA);
+        pastel_chocolate.removerAderezo(Aderezo.CREMA);
+        pastel_chocolate.anadirAderezo(Aderezo.FRUTILLA);
+
         System.out.println(pastel_chocolate);
 //        mnj_leche.cambiarTipoLeche(leche, pastel_chocolate);
         System.out.println(helado_vainilla.showPrecioFinal());
-        
-        
+
     }
 }
